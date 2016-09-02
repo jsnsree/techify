cd96e2bba2f6ee64ffdabaf0180cdde701dbcda3
Sabith Khathim <sabith.m.khathim@gmail.com>
2016-09-02 18:06:24 +0530

    base application first commit

---
+++ application/controllers/Auth.php
@@ -0,0 +1,61 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth extends CI_Controller {
+
+	 
+	public function __construct() {
+		
+        parent::__construct();
+		
+		if($this->aauth->is_loggedin()){
+			
+			redirect('welcome');
+			
+		}
+		
+    }
+	
+	public function index(){
+		
+		$this->login();
+		
+	}
+	
+	public function login($errors = array()){
+		
+		$this->load->view('auth/login_form.php', $errors);
+		
+	}
+	
+	public function do_login(){
+		
+		$username = $this->input->post('username');
+		$password = $this->input->post('password');
+		
+		// check string here
+		
+		$login = $this->aauth->login($username, $password);
+		
+		if($login){
+			
+			redirect('welcome');
+			
+		}else{
+			
+			$errors = $this->aauth->get_errors_array();
+			
+			$this->login(array('errors' => $errors));
+			
+			
+		}
+		
+	}
+	
+	public function create(){
+		
+		var_dump($this->aauth->create_user('jsn@uoc.org', 'jsn@123', 'jsnsree'));
+		
+	}
+	 
+}
