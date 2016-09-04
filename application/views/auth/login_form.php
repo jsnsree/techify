e831f076a795b505616a8ba64319d3ed6dbcd9c2
Sabith Khathim <sabith.m.khathim@gmail.com>
2016-09-04 23:59:33 +0530

    Fixed login page
    #fixed assets folder path. change to absolute path.
    #added error message

--- application/views/auth/login_form.php
+++ application/views/auth/login_form.php
@@ -3,19 +3,17 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
-        <meta name="author" content="Coderthemes">
 
-        <link rel="shortcut icon" href="assets/images/favicon_1.ico">
+        <link rel="shortcut icon" href="/assets/images/favicon.png">
 
-        <title>Techify-login</title>
+        <title>Techify - Login</title>
 
-        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <link href="assets/css/core.css" rel="stylesheet" type="text/css" />
-        <link href="assets/css/components.css" rel="stylesheet" type="text/css" />
-        <link href="assets/css/icons.css" rel="stylesheet" type="text/css" />
-        <link href="assets/css/pages.css" rel="stylesheet" type="text/css" />
-        <link href="assets/css/responsive.css" rel="stylesheet" type="text/css" />
+        <link href="/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="/assets/css/core.css" rel="stylesheet" type="text/css" />
+        <link href="/assets/css/components.css" rel="stylesheet" type="text/css" />
+        <link href="/assets/css/icons.css" rel="stylesheet" type="text/css" />
+        <link href="/assets/css/pages.css" rel="stylesheet" type="text/css" />
+        <link href="/assets/css/responsive.css" rel="stylesheet" type="text/css" />
 
         <!-- HTML5 Shiv and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -24,15 +22,7 @@
         <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
         <![endif]-->
 
-        <script src="assets/js/modernizr.min.js"></script>
-<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-          ga('create', 'UA-69506598-1', 'auto');
-          ga('send', 'pageview');
-        </script>
+        <script src="/assets/js/modernizr.min.js"></script>
         
     </head>
     <body>
@@ -44,11 +34,7 @@
         	<div class=" card-box">
             <div class="panel-heading"> 
     
-               <div class="col-lg-offset-1 col-lg-10">
-                <img src="./assets/images/login.jpg" alt="user-img" class="img-circle img-responsive" height="5px">
-                </div>
-      
- 
+               <div class="col-lg-offset-1 col-lg-10"></div>
                 
             </div> 
 
@@ -58,13 +44,19 @@
                 
                 <div class="form-group ">
                     <div class="col-xs-12">
-                        <input class="form-control" type="text" required="" placeholder="Username">
+                        <input class="form-control" type="text" name="username" required="" placeholder="Username">
                     </div>
                 </div>
 
                 <div class="form-group">
                     <div class="col-xs-12">
-                        <input class="form-control" type="password" required="" placeholder="Password">
+                        <input class="form-control" type="password" name="password" required="" placeholder="Password">  
+                    </div>
+                </div>
+				
+                <div class="form-group">
+                    <div class="col-xs-12 text-danger text-center">
+						<?= !empty($errors) ? $errors[0] : ''; ?>
                     </div>
                 </div>
 
@@ -82,7 +74,6 @@
                 
                 <div class="form-group text-center m-t-40">
                     <div class="col-xs-12">
-                        <center><p class="text-danger"><?= !empty($errors) ? $errors[0] : ''; ?></p></center>
                         <button class="btn btn-pink btn-block text-uppercase waves-effect waves-light" type="submit">Log In</button>
                     </div>
                 </div>
@@ -96,12 +87,13 @@
             
             </div>   
             </div>                              
-                <div class="row">
+            <div class="row">
             	<div class="col-sm-12 text-center">
-            		<p>Don't have an account? <a href="page-register.html" class="text-primary m-l-5"><b>Sign Up</b></a></p>
+            		
+					<p>Don't have an account? <a href="page-register.html" class="text-primary m-l-5"><b>Sign Up</b></a></p>
                         
-                    </div>
-            </div>
+                </div>
+            </div>   
             
         </div>
                 
@@ -113,20 +105,20 @@
         </script>
 
         <!-- jQuery  -->
-        <script src="assets/js/jquery.min.js"></script>
-        <script src="assets/js/bootstrap.min.js"></script>
-        <script src="assets/js/detect.js"></script>
-        <script src="assets/js/fastclick.js"></script>
-        <script src="assets/js/jquery.slimscroll.js"></script>
-        <script src="assets/js/jquery.blockUI.js"></script>
-        <script src="assets/js/waves.js"></script>
-        <script src="assets/js/wow.min.js"></script>
-        <script src="assets/js/jquery.nicescroll.js"></script>
-        <script src="assets/js/jquery.scrollTo.min.js"></script>
+        <script src="/assets/js/jquery.min.js"></script>
+        <script src="/assets/js/bootstrap.min.js"></script>
+        <script src="/assets/js/detect.js"></script>
+        <script src="/assets/js/fastclick.js"></script>
+        <script src="/assets/js/jquery.slimscroll.js"></script>
+        <script src="/assets/js/jquery.blockUI.js"></script>
+        <script src="/assets/js/waves.js"></script>
+        <script src="/assets/js/wow.min.js"></script>
+        <script src="/assets/js/jquery.nicescroll.js"></script>
+        <script src="/assets/js/jquery.scrollTo.min.js"></script>
 
 
-        <script src="assets/js/jquery.core.js"></script>
-        <script src="assets/js/jquery.app.js"></script>
+        <script src="/assets/js/jquery.core.js"></script>
+        <script src="/assets/js/jquery.app.js"></script>
 	
 	</body>
 </html>
