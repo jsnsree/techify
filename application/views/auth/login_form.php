5f57113a0507ff81091dbaf9a9c4961bf59f1e45
jsnsree <jsnsree@gmail.com>
2016-09-04 23:26:03 +0530

    login design
    design change chaithu db ready aaknila

--- application/views/auth/login_form.php
+++ application/views/auth/login_form.php
@@ -1,106 +1,131 @@
-
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <link rel="icon" href="/assets/favicon.png">
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
+        <meta name="author" content="Coderthemes">
 
-    <title>Login</title>
+        <link rel="shortcut icon" href="assets/images/favicon_1.ico">
 
-    <!-- Bootstrap core CSS -->
-    <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+        <title>Techify-login</title>
 
-    <!-- Custom styles for this template -->
-    <style>
-		body {
-			
-			padding-top: 40px;
-			padding-bottom: 40px;
-			background-color: #eee;
-			
-		}
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/core.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/components.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/icons.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/pages.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/responsive.css" rel="stylesheet" type="text/css" />
 
-		.form-signin {
-			
-			max-width: 330px;
-			padding: 15px;
-			margin: 0 auto;
-			
-		}
-		
-		.form-signin .form-signin-heading,
-		.form-signin .checkbox {
-			
-			margin-bottom: 10px;
-			
-		}
-		
-		.form-signin .checkbox {
-			
-			font-weight: normal;
-			
-		}
-		
-		.form-signin .form-control {
-			
-			position: relative;
-			height: auto;
-			-webkit-box-sizing: border-box;
-			-moz-box-sizing: border-box;
-			  box-sizing: border-box;
-			padding: 10px;
-			font-size: 16px;
-			
-		}
-		
-		.form-signin .form-control:focus {
-			z-index: 2;
-		}
-		
-		.form-signin input[type="email"] {
-			margin-bottom: -1px;
-			border-bottom-right-radius: 0;
-			border-bottom-left-radius: 0;
-		}
-		
-		.form-signin input[type="password"] {
-			margin-bottom: 10px;
-			border-top-left-radius: 0;
-			border-top-right-radius: 0;
-		}
-	</style>
+        <!-- HTML5 Shiv and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        <![endif]-->
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
+        <script src="assets/js/modernizr.min.js"></script>
+<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  <body>
+          ga('create', 'UA-69506598-1', 'auto');
+          ga('send', 'pageview');
+        </script>
+        
+    </head>
+    <body>
 
-    <div class="container">
+        <div class="account-pages"></div>
+        <div class="clearfix"></div>
+        <div class="wrapper-page">
+        <h1 class="text-center"><strong class="text-custom">TECHIFY</strong></h1>
+        	<div class=" card-box">
+            <div class="panel-heading"> 
+    
+               <div class="col-lg-offset-1 col-lg-10">
+                <img src=".\assets\images\login.jpg" alt="user-img" class="img-circle img-responsive" height="5px">
+                </div>
+      
+ 
+                
+            </div> 
 
-      <form class="form-signin" action="/auth/do_login" method="post">
-        <h2 class="form-signin-heading">Please sign in</h2>
-        <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" name="username" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
-        <div class="checkbox">
-          <label>
-            <input type="checkbox" value="remember-me"> Remember me
-          </label>
+
+            <div class="panel-body">
+            <form class="form-horizontal m-t-20" action="/auth/do_login" method="post">
+                
+                <div class="form-group ">
+                    <div class="col-xs-12">
+                        <input class="form-control" type="text" required="" placeholder="Username">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-xs-12">
+                        <input class="form-control" type="password" required="" placeholder="Password">
+                    </div>
+                </div>
+
+                <div class="form-group ">
+                    <div class="col-xs-12">
+                        <div class="checkbox checkbox-primary">
+                            <input id="checkbox-signup" type="checkbox">
+                            <label for="checkbox-signup">
+                                Remember me
+                            </label>
+                        </div>
+                        
+                    </div>
+                </div>
+                
+                <div class="form-group text-center m-t-40">
+                    <div class="col-xs-12">
+                        <button class="btn btn-pink btn-block text-uppercase waves-effect waves-light" type="submit">Log In</button>
+                    </div>
+                </div>
+
+                <div class="form-group m-t-30 m-b-0">
+                    <div class="col-sm-12">
+                        <a href="page-recoverpw.html" class="text-dark"><i class="fa fa-lock m-r-5"></i> Forgot your password?</a>
+                    </div>
+                </div>
+            </form> 
+            
+            </div>   
+            </div>                              
+                <div class="row">
+            	<div class="col-sm-12 text-center">
+            		<p>Don't have an account? <a href="page-register.html" class="text-primary m-l-5"><b>Sign Up</b></a></p>
+                        
+                    </div>
+            </div>
+            
         </div>
-		<center><p class="text-danger"><?= !empty($errors) ? $errors[0] : ''; ?></p></center>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-      </form>
+                
+        
 
-    </div> <!-- /container -->
+        
+    	<script>
+            var resizefunc = [];
+        </script>
+
+        <!-- jQuery  -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/detect.js"></script>
+        <script src="assets/js/fastclick.js"></script>
+        <script src="assets/js/jquery.slimscroll.js"></script>
+        <script src="assets/js/jquery.blockUI.js"></script>
+        <script src="assets/js/waves.js"></script>
+        <script src="assets/js/wow.min.js"></script>
+        <script src="assets/js/jquery.nicescroll.js"></script>
+        <script src="assets/js/jquery.scrollTo.min.js"></script>
 
 
-  </body>
+        <script src="assets/js/jquery.core.js"></script>
+        <script src="assets/js/jquery.app.js"></script>
+	
+	</body>
 </html>
