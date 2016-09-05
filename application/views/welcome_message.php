2ae08071e2001bf28931f93b4ba5456f1fc07e57
jsnsree <jsnsree@gmail.com>
2016-09-05 23:12:32 +0530

    welcome page changed
    sub menu added and chandeg page bg

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -180,258 +180,40 @@
 
                 </div>
             </div>
-
-            <div class="navbar-custom">
-                <div class="container">
-                    <div id="navigation">
-                        <!-- Navigation Menu-->
-                        <ul class="navigation-menu">
-                            <li class="has-submenu">
-                                <a href="#"><i class="md md-dashboard"></i>Dashboard</a>
-                                
+<!-- sub nav starting with tabs -->
+            <div class="col-lg-offset-3 col-lg-6">
+                        <ul class="nav nav-tabs tabs" style="width: 100%;">
+                            <li class="active tab" style="width: 25%;">
+                                <a href="#home-2" data-toggle="tab" aria-expanded="false" class="active">
+                                    <span class="visible-xs"><i class="fa fa-home"></i></span>
+                                    <span class="hidden-xs">Home</span>
+                                </a>
                             </li>
-                            <li class="has-submenu">
-                                <a href="#"><i class="md md-color-lens"></i>UI Kit</a>
-                                <ul class="submenu">
-                                    <li><a href="ui-buttons.html">Buttons</a></li>
-                                    <li><a href="ui-loading-buttons.html">Loading Buttons</a></li>
-                                    <li><a href="ui-panels.html">Panels</a></li>
-                                    <li><a href="ui-portlets.html">Portlets</a></li>
-                                    <li><a href="ui-checkbox-radio.html">Checkboxs-Radios</a></li>
-                                    <li><a href="ui-tabs.html">Tabs</a></li>
-                                    <li><a href="ui-modals.html">Modals</a></li>
-                                    <li><a href="ui-progressbars.html">Progress Bars</a></li>
-                                    <li><a href="ui-notification.html">Notification</a></li>
-                                    <li><a href="ui-images.html">Images</a></li>
-                                    <li><a href="ui-carousel.html">Carousel</a>
-                                    <li><a href="ui-video.html">Video</a>
-                                    <li><a href="ui-bootstrap.html">Bootstrap UI</a></li>
-                                    <li><a href="ui-typography.html">Typography</a></li>
-                                </ul>
+                            <li class="tab" style="width: 25%;">
+                                <a href="#profile-2" data-toggle="tab" aria-expanded="false">
+                                    <span class="visible-xs"><i class="fa fa-user"></i></span>
+                                    <span class="hidden-xs">Profile</span>
+                                </a>
                             </li>
-
-
-                            <li class="has-submenu">
-                                <a href="#"><i class="md md-layers"></i>Components</a>
-                                <ul class="submenu megamenu">
-                                    <li>
-                                        <ul>
-                                            <li>
-                                                <span>Elements</span>
-                                            </li>
-                                            <li><a href="components-grid.html">Grid</a></li>
-                                            <li><a href="components-widgets.html">Widgets</a></li>
-                                            <li><a href="components-nestable-list.html">Nesteble</a></li>
-                                            <li><a href="components-range-sliders.html">Range sliders</a></li>
-                                            <li><a href="components-masonry.html">Masonry</a></li>
-                                            <li><a href="components-animation.html">Animation</a></li>
-                                            <li><a href="components-sweet-alert.html">Sweet Alerts</a></li>
-                                            <li><a href="components-treeview.html">Tree view</a></li>
-                                            <li><a href="components-tour.html">Tour</a></li>
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <ul>
-                                            <li>
-                                                <span>Forms</span>
-                                            </li>
-                                            <li><a href="form-elements.html">General Elements</a></li>
-                                            <li><a href="form-advanced.html">Advanced Form</a></li>
-                                            <li><a href="form-validation.html">Form Validation</a></li>
-                                            <li><a href="form-pickers.html">Form Pickers</a></li>
-                                            <li><a href="form-wizard.html">Form Wizard</a></li>
-                                        </ul>
-                                    </li>
-
-                                    <li>
-                                        <ul>
-                                            <li>
-                                                <span>Forms</span>
-                                            </li>
-                                            <li><a href="form-mask.html">Form Masks</a></li>
-                                            <li><a href="form-summernote.html">Summernote</a></li>
-                                            <li><a href="form-wysiwig.html">Wysiwig Editors</a></li>
-                                            <li><a href="form-code-editor.html">Code Editor</a></li>
-                                            <li><a href="form-uploads.html">Multiple File Upload</a></li>
-                                            <li><a href="form-xeditable.html">X-editable</a></li>
-                                            <li><a href="form-image-crop.html">Image Crop</a></li>
-                                        </ul>
-                                    </li>
-
-
-                                </ul>
+                            <li class="tab" style="width: 25%;">
+                                <a href="#messages-2" data-toggle="tab" aria-expanded="true">
+                                    <span class="visible-xs"><i class="fa fa-envelope-o"></i></span>
+                                    <span class="hidden-xs">Messages</span>
+                                </a>
                             </li>
-                            <li class="has-submenu">
-                                <a href="#"><i class="md md-class"></i>Other</a>
-                                <ul class="submenu">
-                                    <li class="has-submenu">
-                                        <a href="#">Tables</a>
-                                        <ul class="submenu">
-                                            <li><a href="tables-basic.html">Basic Tables</a></li>
-                                            <li><a href="tables-datatable.html">Data Table</a></li>
-                                            <li><a href="tables-editable.html">Editable Table</a></li>
-                                            <li><a href="tables-responsive.html">Responsive Table</a></li>
-                                            <li><a href="tables-foo-tables.html">FooTable</a></li>
-                                            <li><a href="tables-bootstrap.html">Bootstrap Tables</a></li>
-                                            <li><a href="tables-tablesaw.html">Tablesaw Tables</a></li>
-                                            <li><a href="tables-jsgrid.html">JsGrid Tables</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class="has-submenu">
-                                        <a href="#">Charts</a>
-                                        <ul class="submenu">
-                                            <li><a href="chart-flot.html">Flot Chart</a></li>
-                                            <li><a href="chart-morris.html">Morris Chart</a></li>
-                                            <li><a href="chart-chartjs.html">Chartjs</a></li>
-                                            <li><a href="chart-peity.html">Peity Charts</a></li>
-                                            <li><a href="chart-chartist.html">Chartist Charts</a></li>
-                                            <li><a href="chart-c3.html">C3 Charts</a></li>
-                                            <li><a href="chart-nvd3.html"> Nvd3 Charts</a></li>
-                                            <li><a href="chart-sparkline.html">Sparkline charts</a></li>
-                                            <li><a href="chart-radial.html">Radial charts</a></li>
-                                            <li><a href="chart-other.html">Other Chart</a></li>
-                                            <li><a href="chart-ricksaw.html">Ricksaw Chart</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class="has-submenu">
-                                        <a href="#">Icons</a>
-                                        <ul class="submenu">
-                                            <li><a href="icons-glyphicons.html">Glyphicons</a></li>
-                                            <li><a href="icons-materialdesign.html">Material Design</a></li>
-                                            <li><a href="icons-ionicons.html">Ion Icons</a></li>
-                                            <li><a href="icons-fontawesome.html">Font awesome</a></li>
-                                            <li><a href="icons-themifyicon.html">Themify Icons</a></li>
-                                            <li><a href="icons-simple-line.html">Simple line Icons</a></li>
-                                            <li><a href="icons-weather.html">Weather Icons</a></li>
-                                            <li><a href="icons-typicons.html">Typicons</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class="has-submenu">
-                                        <a href="#">Maps</a>
-                                        <ul class="submenu">
-                                            <li><a href="map-google.html"> Google Map</a></li>
-                                            <li><a href="map-vector.html"> Vector Map</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class="has-submenu">
-                                        <a href="#">Email</a>
-                                        <ul class="submenu">
-                                            <li><a href="email-inbox.html"> Inbox</a></li>
-                                            <li><a href="email-read.html"> Read Mail</a></li>
-                                            <li><a href="email-compose.html"> Compose Mail</a></li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </li>
-
-                            <li class="has-submenu">
-                                <a href="#"><i class="md md-pages"></i>Pages</a>
-                                <ul class="submenu megamenu">
-                                    <li>
-                                        <ul>
-                                            <li><a href="page-starter.html">Starter Page</a></li>
-                                            <li><a href="page-login.html">Login</a></li>
-                                            <li><a href="page-login-v2.html">Login v2</a></li>
-                                            <li><a href="page-register.html">Register</a></li>
-                                            <li><a href="page-register-v2.html">Register v2</a></li>
-                                            <li><a href="page-signup-signin.html">Signin - Signup</a></li>
-                                            <li><a href="page-recoverpw.html">Recover Password</a></li>
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <ul>
-                                            <li><a href="page-lock-screen.html">Lock Screen</a></li>
-                                            <li><a href="page-400.html">Error 400</a></li>
-                                            <li><a href="page-403.html">Error 403</a></li>
-                                            <li><a href="page-404.html">Error 404</a></li>
-                                            <li><a href="page-404_alt.html">Error 404-alt</a></li>
-                                            <li><a href="page-500.html">Error 500</a></li>
-                                            <li><a href="page-503.html">Error 503</a></li>
-                                        </ul>
-                                    </li>
-
-                                </ul>
-                            </li>
-
-
-                            <li class="has-submenu">
-                                <a href="#"><i class="md md-folder-special"></i>Extras</a>
-                                <ul class="submenu megamenu">
-                                    <li>
-                                        <ul>
-                                            <li><a href="extra-profile.html">Profile</a></li>
-                                            <li><a href="extra-timeline.html">Timeline</a></li>
-                                            <li><a href="extra-sitemap.html">Site map</a></li>
-                                            <li><a href="extra-invoice.html">Invoice</a></li>
-                                            <li><a href="extra-email-template.html">Email template</a></li>
-                                            <li><a href="extra-maintenance.html">Maintenance</a></li>
-                                            <li><a href="extra-coming-soon.html">Coming-soon</a></li>
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <ul>
-                                            <li><a href="extra-faq.html">FAQ</a></li>
-                                            <li><a href="extra-search-result.html">Search result</a></li>
-                                            <li><a href="extra-gallery.html">Gallery</a></li>
-                                            <li><a href="extra-pricing.html">Pricing</a></li>
-                                            <li><a href="apps-calendar.html"> Calendar</a></li>
-                                            <li><a href="apps-contact.html"> Contact</a></li>
-                                            <li><a href="apps-taskboard.html"> Taskboard</a></li>
-                                        </ul>
-                                    </li>
-
-
-                                </ul>
-                            </li>
-
-                            <li class="has-submenu">
-                                <a href="#"><i class="md md-account-circle"></i>CRM</a>
-                                <ul class="submenu">
-                                    <li><a href="crm-dashboard.html"> Dashboard </a></li>
-                                    <li><a href="crm-contact.html"> Contacts </a></li>
-                                    <li><a href="crm-opportunities.html"> Opportunities </a></li>
-                                    <li><a href="crm-leads.html"> Leads </a></li>
-                                    <li><a href="crm-customers.html"> Customers </a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                        <!-- End navigation menu        -->
+                           
+                        <div class="indicator" style="right: 424px; left: 0px;"></div></</ul>
+                        
                     </div>
-                </div> <!-- end container -->
-            </div> <!-- end navbar-custom -->
+              <!-- sub nav ending with tabs -->
         </header>
         <!-- End Navigation Bar-->
 
 
-        <div class="wrapper">
-            <div class="container">
+        
 
-              <!-- Page-Title -->
-                <div class="row">
-                    <div class="col-sm-12">
-                        <!--
-						<div class="btn-group pull-right m-t-15">
-                            <button type="button" class="btn btn-default dropdown-toggle waves-effect" data-toggle="dropdown" aria-expanded="false">Settings <span class="m-l-5"><i class="fa fa-cog"></i></span></button>
-                            <ul class="dropdown-menu drop-menu-right" role="menu">
-                                <li><a href="#">Action</a></li>
-                                <li><a href="#">Another action</a></li>
-                                <li><a href="#">Something else here</a></li>
-                                <li class="divider"></li>
-                                <li><a href="#">Separated link</a></li>
-                            </ul>
-                        </div>
-						-->
-                        <h4 class="page-title">Starter Page</h4>
-                        <ol class="breadcrumb">
-                            <li class="active">
-                                Blank
-                            </li>
-                        </ol>
-                    </div>
-                </div>
-
-                <!-- Demo only -->
-                <div class="" style="height: 800px;"></div>
+                <!-- Demo only >
+                <div class="" style="height: 600px;"></div>
 
                 <!-- Footer -->
                 <footer class="footer text-right">
